b8ddf171dda8261966bffdce128c93c0a46e96b9
student <student@DESKTOP-OT59EC7>
2019-09-21 14:21:03 +0200

    Trzecia proba

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public static class Class1
+    {
+        public static void WriteHello()
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+}
